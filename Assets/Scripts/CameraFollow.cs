15a1b78d3fe543c43568f42a6554c915b9092db0
Levent Kırımgeri <nomengo667@gmail.com>
2020-10-26 17:56:31 +0300

    New Entry

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+
+    [Range(0.01f , 1f)]
+    public float Smoothspeed;
+
+    private Vector3 offset;
+
+    [Tooltip("Enable toLookAt specified target?")]
+    public bool LookAt = false;
+
+    private void Start()
+    {
+        Smoothspeed = 0.5f;
+        offset = transform.position - Target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = Target.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPosition, Smoothspeed);
+
+        if (LookAt)
+        {
+            transform.LookAt(Target);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, 0, -1) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * speed;
+        }
+    }
+}
